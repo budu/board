75da9078a7a64683641fa2d29cc2978e95767bb6
Nicolas Buduroi <nbuduroi@gmail.com>
2012-03-31 18:15:22 -0400

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject board "0.1.0-SNAPSHOT"
   :description "Experimental drawing board"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [aleph "0.2.1-SNAPSHOT"]
+                 [aleph "0.2.1-beta2"]
                  [ring "1.1.0-beta2"]
                  [hiccup "1.0.0-beta1"]
                  [org.clojure/clojurescript "0.0-993"]]
   :source-path "src/clj"
   :extra-classpath-dirs ["src/cljs"]
-  :plugins [[lein-cljsbuild "0.1.4"]]
+  :plugins [[lein-cljsbuild "0.1.5"]]
   :cljsbuild {:builds
               [{:source-path "src/cljs"
                 :compiler {:output-to "resources/public/js/app.js"
